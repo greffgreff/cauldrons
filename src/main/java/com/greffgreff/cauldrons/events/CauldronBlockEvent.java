7df9ac9b6d75fee441c5986e06a9342fc650ac31
greffgreff <greffchandler80@gmail.com>
2022-07-24 20:25:46 +0200

    Added onPlayerInteractEvent event

--- src/main/java/com/greffgreff/cauldrons/events/CauldronBlockEvent.java
+++ src/main/java/com/greffgreff/cauldrons/events/CauldronBlockEvent.java
@@ -1,8 +1,13 @@
 package com.greffgreff.cauldrons.events;
 
 import com.greffgreff.cauldrons.blocks.CauldronBlock;
+import com.greffgreff.cauldrons.utils.Console;
+import net.minecraft.world.item.BucketItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.event.level.BlockEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -10,14 +15,24 @@ import net.minecraftforge.fml.common.Mod;
 @Mod.EventBusSubscriber(Dist.CLIENT)
 public class CauldronBlockEvent {
 
+//    @SubscribeEvent
+//    public static void onBlockPlaceEvent(BlockEvent.EntityPlaceEvent event) {
+//        if (event.getPlacedBlock().getBlock() instanceof CauldronBlock) {
+//            BlockState blockState = event.getPlacedBlock();
+//            blockState.setValue(CauldronBlock.FILL_LEVEL, 1);
+//            Console.debug(blockState);
+//        }
+//    }
+
     @SubscribeEvent
-    public static void onBlockPlaceEvent(BlockEvent.EntityPlaceEvent event) {
+    public static void onPlayerInteractEvent(PlayerInteractEvent event) {
+        BlockState blockState = event.getLevel().getBlockState(event.getPos());
+        ItemStack itemStack = event.getItemStack();
 
-        if (event.getPlacedBlock().getBlock() instanceof CauldronBlock) {
-            BlockState blockState = event.getPlacedBlock();
-
+        if (blockState.getBlock() instanceof CauldronBlock && itemStack.getItem() instanceof BucketItem bucket) {
             blockState.setValue(CauldronBlock.FILL_LEVEL, 1);
+
+            Console.debug(blockState);
         }
     }
-
 }
