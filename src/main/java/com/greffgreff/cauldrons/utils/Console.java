553f65d2428d9052a9a8f5b3b3e90cce2e115a42
greffgreff <greffchandler80@gmail.com>
2022-07-24 19:07:41 +0200

    Added console util class

---
+++ src/main/java/com/greffgreff/cauldrons/utils/Console.java
@@ -0,0 +1,9 @@
+package com.greffgreff.cauldrons.utils;
+
+public final class Console {
+    public static final String PREFIX = "[CAULDRONS]";
+
+    public static void debug(Object obj) {
+        System.out.println(PREFIX + " " + obj.toString());
+    }
+}
