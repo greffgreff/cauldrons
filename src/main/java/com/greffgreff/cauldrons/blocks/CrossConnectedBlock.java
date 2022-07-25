7a3590ac422b9522d8dc2318545d243aeb3e8403
greffgreff <greffchandler80@gmail.com>
2022-07-25 04:08:59 +0200

    Optimized imports

--- src/main/java/com/greffgreff/cauldrons/blocks/CrossConnectedBlock.java
+++ src/main/java/com/greffgreff/cauldrons/blocks/CrossConnectedBlock.java
@@ -1,22 +1,17 @@
 package com.greffgreff.cauldrons.blocks;
 
-import com.greffgreff.cauldrons.utils.Console;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.material.Material;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.List;
