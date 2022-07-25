cec6b07b95dd6069c4e11047768a9c3ca3e59edf
greffgreff <greffchandler80@gmail.com>
2022-07-25 05:32:42 +0200

    Removed connected block

--- src/main/java/com/greffgreff/cauldrons/registries/BlockRegistry.java
+++ src/main/java/com/greffgreff/cauldrons/registries/BlockRegistry.java
@@ -1,6 +1,7 @@
 package com.greffgreff.cauldrons.registries;
 
 import com.greffgreff.cauldrons.Main;
+import com.greffgreff.cauldrons.blocks.CauldronBlock;
 import com.greffgreff.cauldrons.blocks.CrossConnectedBlock;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
@@ -16,8 +17,7 @@ public class BlockRegistry {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, Main.MOD_ID);
     public static final DeferredRegister<Block> GAME_BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, "minecarft");
 
-//    public static final RegistryObject<Block> CAULDRON = registerBlock("cauldron", CauldronBlock::new);
-    public static final RegistryObject<Block> CONNECTED_BLOCK = registerBlock("connected_block", CrossConnectedBlock::new);
+    public static final RegistryObject<Block> CAULDRON = registerBlock("cauldron", CauldronBlock::new);
 
     public static <B extends Block> RegistryObject<B> registerBlock(String name, Supplier<B> supplier) {
         RegistryObject<B> block = BLOCKS.register(name, supplier);
