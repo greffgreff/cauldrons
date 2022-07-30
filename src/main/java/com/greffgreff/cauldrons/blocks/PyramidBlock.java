5b8f7390660bef43c6411e67ecb9b2481df27704
greffgreff <greffchandler80@gmail.com>
2022-07-30 17:28:00 +0200

    Made pyramid block water loggable

--- src/main/java/com/greffgreff/cauldrons/blocks/PyramidBlock.java
+++ src/main/java/com/greffgreff/cauldrons/blocks/PyramidBlock.java
@@ -3,18 +3,29 @@ package com.greffgreff.cauldrons.blocks;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SimpleWaterloggedBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.level.material.FluidState;
+import net.minecraft.world.level.material.Fluids;
 import net.minecraft.world.level.material.Material;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
 @SuppressWarnings("NullableProblems")
-public class PyramidBlock extends Block {
-    private static final VoxelShape SHAPE = Block.box(0,0,0, 16, 8, 16);
+public class PyramidBlock extends Block implements SimpleWaterloggedBlock {
+    public static final BooleanProperty WATER_LOGGED = BlockStateProperties.WATERLOGGED;
+    private static final VoxelShape SHAPE = Block.box(0, 0, 0, 16, 8, 16);
 
     public PyramidBlock() {
         super(Properties.of(Material.STONE).noOcclusion());
+
+        registerDefaultState(this.stateDefinition.any()
+                .setValue(WATER_LOGGED, false)
+        );
     }
 
     @Override
@@ -22,4 +33,13 @@ public class PyramidBlock extends Block {
         return SHAPE;
     }
 
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(WATER_LOGGED);
+    }
+
+    @Override
+    public FluidState getFluidState(BlockState blockState) {
+        return blockState.getValue(WATER_LOGGED) ? Fluids.WATER.getSource(false) : super.getFluidState(blockState);
+    }
 }
