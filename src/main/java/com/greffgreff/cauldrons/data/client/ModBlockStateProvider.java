6af7693d4aa7466419fafabf60b24efb46e9888a
greffgreff <greffchandler80@gmail.com>
2022-07-30 04:41:45 +0200

    Removed redundant util class

--- src/main/java/com/greffgreff/cauldrons/data/client/ModBlockStateProvider.java
+++ src/main/java/com/greffgreff/cauldrons/data/client/ModBlockStateProvider.java
@@ -3,8 +3,6 @@ package com.greffgreff.cauldrons.data.client;
 import com.greffgreff.cauldrons.Main;
 import com.greffgreff.cauldrons.blocks.CrossConnectedBlock;
 import com.greffgreff.cauldrons.registries.BlockRegistry;
-import com.greffgreff.cauldrons.utils.DirectionalUtil;
-import it.unimi.dsi.fastutil.Pair;
 import net.minecraft.core.Direction;
 import net.minecraft.data.DataGenerator;
 import net.minecraftforge.client.model.generators.BlockModelBuilder;
@@ -13,8 +11,6 @@ import net.minecraftforge.client.model.generators.MultiPartBlockStateBuilder;
 import net.minecraftforge.common.data.ExistingFileHelper;
 
 import static com.greffgreff.cauldrons.blocks.CrossConnectedBlock.*;
-import static com.greffgreff.cauldrons.utils.DirectionalUtil.getAdjacentSides;
-import static com.greffgreff.cauldrons.utils.DirectionalUtil.getRelativeRotation;
 
 @SuppressWarnings("ConstantConditions")
 public class ModBlockStateProvider extends BlockStateProvider {
@@ -40,29 +36,33 @@ public class ModBlockStateProvider extends BlockStateProvider {
                 .   condition(CrossConnectedBlock.DOWN, false)
                 .end();
 
-        for (Direction direction: DirectionalUtil.getHorizontalDirections()) {
-            Pair<Direction, Direction> adjacentSides = getAdjacentSides(direction);
+        for (Direction direction: Direction.values()) {
+            if (direction == Direction.DOWN || direction == Direction.UP) {
+                continue;
+            }
 
-            // TODO - make use of Direction::toYRot
+            Direction right = direction.getClockWise();
+            Direction left = direction.getCounterClockWise();
+
             builder
                     .part()
-                    .   modelFile(sideModel).rotationY(getRelativeRotation(direction)).addModel()
+                    .   modelFile(sideModel).rotationY((int) direction.getOpposite().toYRot()).addModel()
                     .   condition(getProperty(direction), false)
                     .end().part()
-                    .   modelFile(angleModel).rotationY(getRelativeRotation(adjacentSides.right())).addModel()
-                    .   condition(getAngleProperty(adjacentSides.right(), direction), false)
+                    .   modelFile(angleModel).rotationY((int) right.getOpposite().toYRot()).addModel()
+                    .   condition(getAngleProperty(right, direction), false)
                     .   condition(getProperty(direction), true)
-                    .   condition(getProperty(adjacentSides.right()), true)
+                    .   condition(getProperty(right), true)
                     .end().part()
-                    .   modelFile(angleModel).rotationY(getRelativeRotation(adjacentSides.left()) + 90).addModel()
-                    .   condition(getAngleProperty(adjacentSides.left(), direction), false)
+                    .   modelFile(angleModel).rotationY((int) left.getOpposite().toYRot() + 90).addModel()
+                    .   condition(getAngleProperty(left, direction), false)
                     .   condition(getProperty(direction), true)
-                    .   condition(getProperty(adjacentSides.left()), true)
+                    .   condition(getProperty(left), true)
                     .end().part()
-                    .   modelFile(angleModel).rotationY(getRelativeRotation(adjacentSides.right())).addModel()
+                    .   modelFile(angleModel).rotationY((int) right.getOpposite().toYRot()).addModel()
                     .   condition(getProperty(direction), false)
                     .end().part()
-                    .   modelFile(angleModel).rotationY(getRelativeRotation(adjacentSides.left()) + 90).addModel()
+                    .   modelFile(angleModel).rotationY((int) left.getOpposite().toYRot() + 90).addModel()
                     .   condition(getProperty(direction), false)
                     .end();
         }
